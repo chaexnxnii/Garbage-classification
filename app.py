dad1b6dace2311dabdd5955a616337a7ba35c0cf
chaexnxnii <113587113+chaexnxnii@users.noreply.github.com>
2023-06-25 20:02:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
-import streamlit as st
-from tensorflow.keras.models import load_model
-from PIL import Image
-import numpy as np
+# import streamlit as st
+# from tensorflow.keras.models import load_model
+# from PIL import Image
+# import numpy as np
 
-st.title("Image Classifier")
+# st.title("Image Classifier")
 
-model = load_model('saved_model.pb')  # Load the model
+# model = load_model('saved_model.pb')  # Load the model
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])  # Image uploader
+# uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])  # Image uploader
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
+# if uploaded_file is not None:
+#     image = Image.open(uploaded_file)
+#     st.image(image, caption='Uploaded Image.', use_column_width=True)
 
-    # Preprocess the image here (reshape, normalize, etc.)
-    # Assuming the function 'preprocess' does that
-    processed_image = preprocess(image)
+#     # Preprocess the image here (reshape, normalize, etc.)
+#     # Assuming the function 'preprocess' does that
+#     processed_image = preprocess(image)
 
-    # Make a prediction
-    predictions = model.predict(np.array([processed_image]))
-    predicted_class = np.argmax(predictions)
-    st.write(f"Predicted Class: {predicted_class}")
+#     # Make a prediction
+#     predictions = model.predict(np.array([processed_image]))
+#     predicted_class = np.argmax(predictions)
+#     st.write(f"Predicted Class: {predicted_class}")
 
 
 
